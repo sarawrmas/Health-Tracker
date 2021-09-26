3f356cad5c205ce585286eefb8951863cff76fc7
Sara <sara.m.adamski@gmail.com>
2021-09-26 12:55:48 -0500

    initialize back end server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
